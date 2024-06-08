dd5f6b138fabfc9e270bd5e07024b1725beec858
MINJAE <mjpark1124@gmail.com>
2024-06-08 19:12:36 +0900

    alarm_complet

--- Umm/src/component/Screen2.js
+++ Umm/src/component/Screen2.js
@@ -1,88 +1,107 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import imgTrash from "../img/trashbag.png";
 
-const screen = {
-    display: "flex",
-    justifyContent: "space-between",
-};
-
-const mapContainer = {
-    width: "50%",
-    border: "1px solid black",
-    display: "flex",
-    flexDirection: "column",
-};
-
-const locateContent = {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    margin: "2%",
-};
-
-const locateBar = {
-    border: "1px solid black",
-    borderRadius: "5px",
-    width: "80%",
-    boxSizing: "content-box",
-    margin: "2%",
-};
-
-const mapContent = {
-    width: "90%",
-    height: "90%",
-    border: "5px solid rgb(30, 187, 72)",
-    margin: "2%",
-    borderRadius: "5px",
-};
-
-// -----------------------------------------
-
-const dataContainer = {
-    width: "45%",
-    border: "1px solid black",
-};
-
-const barList = {
-    listStyleType: "none",
-    padding: "0",
-    display: "flex",
-    justifyContent: "center",
-};
-
-const barItem = {
-    marginBottom: "10px",
-    backgroundColor: "rgb(30, 187, 72)",
-    width: "70%",
-    height: "3rem",
-    borderRadius: "5px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-};
-
-const select = {
-    width: "30%",
-    marginBottom: "10px",
-    padding: "8px",
-    border: "1px solid #ccc",
-    borderRadius: "5px",
-    backgroundColor: "#fff",
-    margin: "2%",
-};
-
-const imgicon = {
-    height: "40px",
-    width: "40px",
-};
-
-const pstyle = {
-    fontWeight: "bold",
-    marginLeft: "5%",
-    marginRight: "5%",
+const styles = {
+    screen: {
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    mapContainer: {
+        width: "50%",
+        border: "1px solid black",
+        display: "flex",
+        flexDirection: "column",
+    },
+    locateContent: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        margin: "2%",
+    },
+    locateBar: {
+        border: "1px solid black",
+        borderRadius: "5px",
+        width: "80%",
+        boxSizing: "content-box",
+        margin: "2%",
+    },
+    mapContent: {
+        width: "90%",
+        height: "90%",
+        border: "5px solid rgb(30, 187, 72)",
+        margin: "2%",
+        borderRadius: "5px",
+    },
+    dataContainer: {
+        width: "45%",
+        border: "1px solid black",
+    },
+    barList: {
+        listStyleType: "none",
+        padding: "0",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+    },
+    barItem: {
+        marginBottom: "10px",
+        backgroundColor: "rgb(30, 187, 72)",
+        width: "70%",
+        height: "3rem",
+        borderRadius: "5px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    select: {
+        width: "30%",
+        marginBottom: "10px",
+        padding: "8px",
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        backgroundColor: "#fff",
+        margin: "2%",
+    },
+    imgIcon: {
+        height: "40px",
+        width: "40px",
+    },
+    pStyle: {
+        fontWeight: "bold",
+        marginLeft: "5%",
+        marginRight: "5%",
+    },
 };
 
 function Screen2() {
+    const [userLocation, setUserLocation] = useState("");
+    const [locations, setLocations] = useState([]);
+
+    useEffect(() => {
+        const email = "user@example.com"; // replace with the actual email
+
+        axios
+            .get(`http://localhost:3001/userlocation?email=${email}`)
+            .then((response) => {
+                console.log("사용자 위치 데이터:", response.data);
+                setUserLocation(response.data.location);
+            })
+            .catch((error) => {
+                console.error("사용자 위치를 가져오는 중 에러 발생:", error);
+            });
+
+        axios
+            .get("http://localhost:3001/locations")
+            .then((response) => {
+                console.log("장소 데이터:", response.data);
+                setLocations(response.data);
+            })
+            .catch((error) => {
+                console.error("장소 데이터를 가져오는 중 에러 발생:", error);
+            });
+    }, []);
+
     useEffect(() => {
         const script = document.createElement("script");
         script.src =
@@ -92,110 +111,116 @@ function Screen2() {
 
         script.onload = () => {
             if (window.kakao && window.kakao.maps) {
-                const infowindow = new window.kakao.maps.InfoWindow({
-                    zIndex: 1,
-                });
-
-                const mapContainer = document.getElementById("map"); // 지도를 표시할 div
-                const mapOption = {
-                    center: new window.kakao.maps.LatLng(
-                        36.1460625,
-                        128.3934375
-                    ), // 지도의 중심좌표
-                    level: 3, // 지도의 확대 레벨
-                };
-
-                const map = new window.kakao.maps.Map(mapContainer, mapOption);
-
-                const ps = new window.kakao.maps.services.Places();
-
-                const keyword = "동대구역";
-
-                ps.keywordSearch(keyword, placesSearchCB);
-
-                function placesSearchCB(data, status, pagination) {
-                    if (status === window.kakao.maps.services.Status.OK) {
-                        const bounds = new window.kakao.maps.LatLngBounds();
-
-                        for (let i = 0; i < data.length; i++) {
-                            displayMarker(data[i]);
-                            bounds.extend(
-                                new window.kakao.maps.LatLng(
-                                    data[i].y,
-                                    data[i].x
-                                )
-                            );
-                        }
-
-                        map.setBounds(bounds);
-                    }
-                }
-
-                function displayMarker(place) {
-                    const marker = new window.kakao.maps.Marker({
-                        map: map,
-                        position: new window.kakao.maps.LatLng(
-                            place.y,
-                            place.x
-                        ),
-                    });
-
-                    window.kakao.maps.event.addListener(
-                        marker,
-                        "click",
-                        function () {
-                            infowindow.setContent(
-                                '<div style="padding:5px;font-size:12px;">' +
-                                    place.place_name +
-                                    "</div>"
-                            );
-                            infowindow.open(map, marker);
-                        }
-                    );
-                }
+                initializeMap();
             } else {
                 console.error("카카오 지도 API를 로드할 수 없습니다.");
             }
         };
-    }, []);
+
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, [userLocation, locations]);
+
+    const initializeMap = () => {
+        const infowindow = new window.kakao.maps.InfoWindow({
+            zIndex: 1,
+        });
+
+        const mapContainer = document.getElementById("map");
+        const mapOption = {
+            center: new window.kakao.maps.LatLng(36.1460625, 128.3934375),
+            level: 3,
+        };
+
+        const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+        if (userLocation) {
+            const ps = new window.kakao.maps.services.Places();
+            ps.keywordSearch(userLocation, (data, status) => {
+                if (status === window.kakao.maps.services.Status.OK) {
+                    const bounds = new window.kakao.maps.LatLngBounds();
+                    data.forEach((place) => {
+                        displayMarker(map, infowindow, place);
+                        bounds.extend(
+                            new window.kakao.maps.LatLng(place.y, place.x)
+                        );
+                    });
+                    map.setBounds(bounds);
+                } else {
+                    console.error("사용자 위치 검색 실패:", status);
+                }
+            });
+        }
+
+        locations.forEach((location) => {
+            const marker = new window.kakao.maps.Marker({
+                map: map,
+                position: new window.kakao.maps.LatLng(
+                    location.latitude,
+                    location.longitude
+                ),
+                title: location.locationName,
+            });
+
+            window.kakao.maps.event.addListener(marker, "click", () => {
+                infowindow.setContent(
+                    '<div style="padding:5px;font-size:12px;">' +
+                        location.locationName +
+                        "</div>"
+                );
+                infowindow.open(map, marker);
+            });
+        });
+    };
+
+    const displayMarker = (map, infowindow, place) => {
+        const marker = new window.kakao.maps.Marker({
+            map: map,
+            position: new window.kakao.maps.LatLng(place.y, place.x),
+        });
+
+        window.kakao.maps.event.addListener(marker, "click", () => {
+            infowindow.setContent(
+                '<div style="padding:5px;font-size:12px;">' +
+                    place.place_name +
+                    "</div>"
+            );
+            infowindow.open(map, marker);
+        });
+    };
 
     return (
-        <div className="screen" style={screen}>
-            <div className="map-container" style={mapContainer}>
+        <div className="screen" style={styles.screen}>
+            <div className="map-container" style={styles.mapContainer}>
                 <div>
-                    <label style={locateContent}>
+                    <label style={styles.locateContent}>
                         내위치:
-                        <div style={locateBar}>
-                            <p>인천광역시 서구 검단동</p>
+                        <div style={styles.locateBar}>
+                            <p>{userLocation || "로딩 중..."}</p>
                         </div>
                     </label>
                 </div>
-                <div id="map" style={mapContent}></div>
+                <div id="map" style={styles.mapContent}></div>
             </div>
 
-            {/* Right section with data from Excel */}
-            <div className="data-container" style={dataContainer}>
-                {/* Select element for choosing the type of list */}
-                <select style={select}>
+            <div className="data-container" style={styles.dataContainer}>
+                <select style={styles.select}>
                     <option value="type1">수거 용기</option>
                     <option value="type2">전용 봉투</option>
-                    {/* Add more options as needed */}
                 </select>
 
-                {/* List of bar-shaped data */}
-                <ul className="bar-list" style={barList}>
-                    {/* Each list item represents a bar-shaped data */}
-                    <li className="bar-item" style={barItem}>
+                <ul className="bar-list" style={styles.barList}>
+                    <li className="bar-item" style={styles.barItem}>
                         <img
-                            style={imgicon}
+                            style={styles.imgIcon}
                             src={imgTrash}
                             alt="trash icon"
                         ></img>
-                        <p style={pstyle}>전용봉투</p>
-                        <p style={pstyle}>2L</p>
-                        <p style={pstyle}>400</p>
+                        <p style={styles.pStyle}>전용봉투</p>
+                        <p style={styles.pStyle}>2L</p>
+                        <p style={styles.pStyle}>400</p>
                     </li>
-                    {/* Add more list items as needed */}
                 </ul>
             </div>
         </div>
--- Umm/src/component/Screen3.js
+++ Umm/src/component/Screen3.js
@@ -2,18 +2,24 @@ import React, { useState, useEffect } from "react";
 
 const screen = {
     display: "flex",
-    justifyContent: "space-between",
 };
 
 const alarmContent = {
     display: "flex",
     flexDirection: "column",
-    width: "50%",
+    alignItems: "center",
+    width: "450px",
 };
 
-const inputStyle = {
+const selectBar = {
+    display: "flex",
+    justifyContent: "center",
+    height: "45px", // 높이를 1.5배로 키움
+};
+
+const selectStyle = {
     margin: "5px",
-    width: "70px",
+    width: "100px",
 };
 
 const addButton = {
@@ -22,7 +28,8 @@ const addButton = {
     color: "white",
     border: "none",
     borderRadius: "5px",
-    padding: "8px 16px",
+    padding: "4px 8px",
+    width: "150px", // 버튼 너비를 150px로 설정
 };
 
 const addButtonContainer = {
@@ -32,29 +39,43 @@ const addButtonContainer = {
     alignItems: "center",
 };
 
-const alarmList = {
-    display: "flex",
-    flexDirection: "column",
-};
-
 const alarmItem = {
     display: "flex",
     alignItems: "center",
-    marginBottom: "10px",
+    marginBottom: "20px",
+    marginTop: "20px",
+    height: "100px",
+    width: "300px",
+    border: "2px solid rgb(30, 187, 72)",
+    paddingLeft: "8px", // 왼쪽 패딩 설정
+    paddingRight: "8px", // 오른쪽 패딩 설정
+    fontWeight: "bold", // 글씨를 굵게 설정
 };
 
 const removeButton = {
-    padding: "5px 10px",
+    padding: "2.5px 5px",
     border: "none",
-    backgroundColor: "gray",
+    backgroundColor: "rgb(30, 187, 72)",
     color: "white",
     cursor: "pointer",
+    width: "80px", // 버튼 너비를 80px로 설정
+    float: "right", // 버튼을 오른쪽으로 정렬
+    fontWeight: "bold", // 글씨를 굵게 설정
 };
+
+const daysOfWeek = [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+];
 
 const Screen3 = () => {
     const [alarms, setAlarms] = useState([]);
     const [alarmTime, setAlarmTime] = useState({
-        month: "",
         day: "",
         hour: "",
         minute: "",
@@ -65,38 +86,40 @@ const Screen3 = () => {
             const currentTime = new Date();
 
             alarms.forEach((alarm) => {
-                const notificationTime = new Date(alarm.time);
+                const [day, time] = alarm.time.split(" ");
+                const [hour, minute] = time.split(":");
 
                 if (
-                    currentTime.getHours() === notificationTime.getHours() &&
-                    currentTime.getMinutes() === notificationTime.getMinutes()
+                    currentTime.getDay() === parseInt(day, 10) &&
+                    currentTime.getHours() === parseInt(hour, 10) &&
+                    currentTime.getMinutes() === parseInt(minute, 10)
                 ) {
-                    // Fire notification or trigger desired action here
+                    // 여기서 알림을 실행하거나 원하는 동작을 트리거합니다.
                     console.log("Alarm triggered:", alarm.time);
                 }
             });
-        }, 1000); // Check every second
+        }, 1000); // 매 초마다 체크
 
         return () => clearInterval(interval);
     }, [alarms]);
 
     const handleAddAlarm = () => {
-        if (alarms.length >= 6) {
-            alert("알람은 최대 6개만 등록 가능.");
+        if (alarms.length >= 4) {
+            alert("알람은 최대 4개만 등록 가능.");
             return;
         }
 
-        const { month, day, hour, minute } = alarmTime;
-        if (month !== "" && day !== "" && hour !== "" && minute !== "") {
-            const formattedTime = `${month}/${day} ${hour}:${minute}`;
+        const { day, hour, minute } = alarmTime;
+        if (day !== "" && hour !== "" && minute !== "") {
+            const formattedTime = `${day} ${hour}:${minute}`;
             const newAlarm = {
-                id: Date.now(), // Using timestamp as ID
+                id: Date.now(), // 타임스탬프를 ID로 사용
                 time: formattedTime,
             };
             setAlarms((prevAlarms) => [...prevAlarms, newAlarm]);
-            setAlarmTime({ month: "", day: "", hour: "", minute: "" });
+            setAlarmTime({ day: "", hour: "", minute: "" });
         } else {
-            alert("Please fill in all fields to set the alarm.");
+            alert("모든 필드를 채워 알람을 설정해주세요.");
         }
     };
 
@@ -110,62 +133,80 @@ const Screen3 = () => {
         setAlarmTime({ ...alarmTime, [name]: value });
     };
 
+    const formatAlarmTime = (alarm) => {
+        const [day, time] = alarm.time.split(" ");
+        const [hour, minute] = time.split(":");
+        return `${daysOfWeek[parseInt(day, 10)]} ${hour}시 ${minute}분`;
+    };
+
     return (
         <div className="screen" style={screen}>
             <div style={alarmContent}>
-                <div>
-                    <input
-                        style={inputStyle}
-                        type="text"
-                        name="month"
-                        value={alarmTime.month}
-                        onChange={handleInputChange}
-                        placeholder="Month"
-                    />
-                    /
-                    <input
-                        style={inputStyle}
-                        type="text"
+                <div className="select" style={selectBar}>
+                    <select
+                        style={selectStyle}
                         name="day"
                         value={alarmTime.day}
                         onChange={handleInputChange}
-                        placeholder="Day"
-                    />
-                    <input
-                        style={inputStyle}
-                        type="text"
+                    >
+                        <option value="">요일</option>
+                        {[...Array(7).keys()].map((d) => (
+                            <option key={d} value={d}>
+                                {daysOfWeek[d]}
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        style={selectStyle}
                         name="hour"
                         value={alarmTime.hour}
                         onChange={handleInputChange}
-                        placeholder="Hour"
-                    />
+                    >
+                        <option value="">시간</option>
+                        {[...Array(24).keys()].map((h) => (
+                            <option key={h} value={h}>
+                                {h.toString().padStart(2, "0")}
+                            </option>
+                        ))}
+                    </select>
                     :
-                    <input
-                        style={inputStyle}
-                        type="text"
+                    <select
+                        style={selectStyle}
                         name="minute"
                         value={alarmTime.minute}
                         onChange={handleInputChange}
-                        placeholder="Minute"
-                    />
+                    >
+                        <option value="">분</option>
+                        {[...Array(60).keys()].map((m) => (
+                            <option key={m} value={m}>
+                                {m.toString().padStart(2, "0")}
+                            </option>
+                        ))}
+                    </select>
                 </div>
                 <div style={addButtonContainer}>
                     <button style={addButton} onClick={handleAddAlarm}>
                         +
                     </button>
-                    <p>
-                        <strong>Add Alarm</strong>
+                    <p style={{ marginLeft: "10px" }}>
+                        <strong>알람 추가</strong>
                     </p>
                 </div>
-                <div style={alarmList}>
+                <div>
                     {alarms.map((alarm) => (
-                        <div key={alarm.id} style={alarmItem}>
-                            <p>{alarm.time}</p>
+                        <div
+                            key={alarm.id}
+                            style={{
+                                ...alarmItem,
+                                justifyContent: "space-between",
+                            }}
+                        >
+                            <p>{formatAlarmTime(alarm)}</p>
                             <button
                                 style={removeButton}
                                 onClick={() => handleRemoveAlarm(alarm.id)}
                             >
-                                Remove
+                                알람 삭제
                             </button>
                         </div>
                     ))}
